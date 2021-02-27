d61aca30ac0d8979f2bda4672a4c4856da3d2cd4
Theodor Khademi <theo.khademi@gmail.com>
2021-02-27 18:38:41 +0100

    Own webcam and microphone now streaming
    Co-authored-by:  Daniel Karlkvist <danielkarlkvist@gmail.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,9 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { VideoStreamer } from './components/VideoStreamer';
+
 function App() {
     return (
-        <div className="App">
+        <div>
+            <VideoStreamer/>
+        </div>
+        
+        /*<div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>Edit <code>src/App.tsx</code> and save to reload.</p> 
@@ -15,7 +21,7 @@ function App() {
                     rel="noopener noreferrer"
                 >Learn React</a>
             </header>
-        </div>
+        </div>*/
     );
 }
 
---
+++ client/src/components/VideoStreamer.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+export class VideoStreamer extends React.Component {
+    constructor(props:any) {
+        super(props);
+        this.streamCamVideo = this.streamCamVideo.bind(this)
+    }
+
+    streamCamVideo() {
+        var constraints = { audio: true, video: { width: 1280, height: 720 } };
+        navigator.mediaDevices
+          .getUserMedia(constraints)
+          .then(function(mediaStream) {
+            var video = document.querySelector("video");
+
+            if (video != null) {
+                video.srcObject = mediaStream;
+                video.onloadedmetadata = function(e) {
+                if (video != null) {
+                    video.play();
+                }
+                };
+            }
+          })
+          .catch(function(err) {
+            console.log(err.name + ": " + err.message);
+          }); // always check for errors at the end.
+      }
+      render() {
+        return (
+          <div>
+            <div id="container">
+              <video autoPlay={true} id="videoElement" controls></video>
+            </div>
+            <br/>
+            <button onClick={this.streamCamVideo}>Start streaming</button>
+          </div>
+        );
+      }
+}
