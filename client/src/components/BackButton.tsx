d60e0d9d0cf3ddc673d5829e5f99b4be4866c37d
Charlie Lam <gusjlakjch@student.gu.se>
2021-03-15 16:20:01 +0100

    delete account button and back button

---
+++ client/src/components/BackButton.tsx
@@ -0,0 +1,23 @@
+ /* Component for go back one step in the directory
+ Author: Charlie and Hanna 
+ */
+
+ import React from 'react';
+ import '../css/profile.css';
+ 
+ interface Props {
+     buttonFunction:Function;
+   }
+ 
+ export const BackButton = (props:Props) => {
+
+     return (
+         <div className="back-button-container">
+             <button className="back-button" onClick={() => props.buttonFunction()}> 
+                 <h2>Tillbaka</h2>
+             </button>
+         </div>         
+     );
+ }
+ 
+ 
--- client/src/pages/ProfilePage.tsx
+++ client/src/pages/ProfilePage.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import '../css/profile.css';
 import hjordis from "../images/hjordis.png"
+import trashbin from "../icons/profile/trashbin.svg"
 import { UserName } from '../Types';
 import { ProfileChangeButton } from '../components/ProfileChangeButton';
 
@@ -8,6 +9,7 @@ import changeName from "../icons/profile/changeName.svg"
 import changeNumber from "../icons/profile/changeNumber.svg"
 import changePassword from "../icons/profile/changePassword.svg"
 import changePicture from "../icons/profile/changePicture.svg"
+import { BackButton } from '../components/BackButton';
 
 interface Props {
     userName: UserName
@@ -27,13 +29,18 @@ export const ProfilePage = (props: Props) => {
         <div>
             {/* Header for 'tillbaka', 'Min profil' and 'Ta bort konto' */}
             <header className="profile-header-container profile-header">
-                <button>Tillbaka</button>
+                <div className="back-button-container">
+                    <BackButton buttonFunction={ButtonNameClicked} />
+                </div>
                 <h1>Min profil</h1>
-                <button>Ta bort konto</button>
+                    <button className="delete-account-container">
+                        <div className="item-delete-icon"><img src={trashbin} alt="trashbin" /></div>
+                        <header className="item-delete-header">Ta bort konto</header>    
+                    </button>
             </header>
-            {/* Container for profil pic, name and number */}
+            {/* Container for profile pic, name and number */}
             <div className="profile-big-info-container">
-                <div className="profile-info-picture-container"><img src={hjordis} alt="profilbild"/></div>
+                <img src={hjordis} alt="profilbild"/>
                 <div className="profile-info-contact-container">
                     <h1 className="profile-name">{props.userName}</h1>
                     <h1 className="profile-name">070123456789</h1>
