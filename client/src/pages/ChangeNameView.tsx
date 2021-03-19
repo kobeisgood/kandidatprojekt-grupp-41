259ac5a663974fc7f7400466a3b88db32c6e65ea
Charlie Lam <gusjlakjch@student.gu.se>
2021-03-19 15:02:44 +0100

    Linked "Ändra namn" button to change name view
    Co-authored-by: Hanna Kalldal <hanna@kalldal.se>

--- client/src/pages/ChangeNameView.tsx
+++ client/src/pages/ChangeNameView.tsx
@@ -2,17 +2,18 @@
 Authors: Charlie and Hanna 
 */
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import '../css/profile.css';
 import hjordis from "../images/hjordis.png"
-import { UserName } from '../Types';
+import { User } from '../Types';
 
 import { BackButton } from '../components/BackButton';
 
 interface Props {
-    userName: UserName
+    user: User
 }
 
-export const ProfileView = (props: Props) => {
+export const ChangeNameView = (props: Props) => {
     // Event handler for clicking back button and the change name button etc. 
     const ButtonNameClicked = () => {
 
@@ -22,7 +23,9 @@ export const ProfileView = (props: Props) => {
         <div>
             <header className="profile-header-container profile-header">
                 <div className="back-button-container">
-                    <BackButton buttonFunction={ButtonNameClicked} />
+                    <Link to="/profile">
+                        <BackButton buttonFunction={ButtonNameClicked} />
+                    </Link>
                 </div>
                 <h1 className="profile-header">Min profil</h1>
             </header>
@@ -30,7 +33,7 @@ export const ProfileView = (props: Props) => {
             <div className="profile-big-info-container">
                 <img src={hjordis} alt="profilbild" />
                 <div className="profile-info-contact-container">
-                    <h1 className="profile-name">{props.userName}</h1>
+                    {/* <h1 className="profile-name">{props.user.firstName + " " + props.user.lastName}</h1> */}
                     <h1 className="profile-number">0701234567</h1>
                 </div>
             </div>
