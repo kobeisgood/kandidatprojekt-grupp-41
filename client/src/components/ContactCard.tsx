755cbb27fdca81cb60cb9fe4096389e09e1c10e6
Daniel Karlkvist <danielkarlkvist@gmail.com>
2021-03-21 13:20:19 +0100

    Fixed styling in contact card
    Co-authored-by: Robin Repo Wecklauf <robinrepowecklauf@gmail.com>

--- client/src/components/ContactCard.tsx
+++ client/src/components/ContactCard.tsx
@@ -1,21 +1,24 @@
 
- import React from 'react';
- import '../css/contact-card.css';
- 
+import React from 'react';
+import '../css/contact-card.css';
+import '../css/colors.css';
+import CallIcon from '../icons/call-icon.svg';
 
- 
- export const ContactCard = () => {
-     return (
+export const ContactCard = () => {
+    return (
         <div className="contact-card-container">
             <div className="contact-card-flexbox">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Edward_blom.melodifestivalen2018.18d873.1460307.jpg/1200px-Edward_blom.melodifestivalen2018.18d873.1460307.jpg" alt="Profilbild"/>
+                <img className="contact-card-profile-picture" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Edward_blom.melodifestivalen2018.18d873.1460307.jpg/1200px-Edward_blom.melodifestivalen2018.18d873.1460307.jpg" alt="Profilbild" />
                 <p className="contact-name">Repo <span>Laufsson</span></p>
                 <button className="call-button">
-                    Ring
+                    <div className="call-button-flexbox">
+                        <img className="call-button-icon" src={CallIcon} alt="Ringikon" />
+                        <p className="call-button-text">Ring</p>
+                    </div>
                 </button>
             </div>
         </div>
-     );
- }
- 
- 
+    );
+}
+
+
