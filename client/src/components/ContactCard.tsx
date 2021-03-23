1d562cc01e85b7de00ffa3dafa3b65ca349d24a3
Daniel Karlkvist <danielkarlkvist@gmail.com>
2021-03-23 19:16:38 +0100

    Changed image to hjordis in contact card

--- client/src/components/ContactCard.tsx
+++ client/src/components/ContactCard.tsx
@@ -7,12 +7,13 @@ import '../css/contact-card.css';
 import '../css/colors.css';
 import callIcon from '../icons/call-icon.svg';
 import { SquareButton } from './SquareButton';
+import hjordis from '../images/hjordis.jpg';
 
 export const ContactCard = () => {
     return (
         <div className="contact-card-container">
             <div className="contact-card-flexbox">
-                <img className="contact-card-profile-picture" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Edward_blom.melodifestivalen2018.18d873.1460307.jpg/1200px-Edward_blom.melodifestivalen2018.18d873.1460307.jpg" alt="Profilbild" />
+                <img className="contact-card-profile-picture" src={hjordis} alt="Profilbild" />
                 <p className="contact-name">Repo <span>Laufsson</span></p>
                 {/* TODO
                     - Add call function on click
