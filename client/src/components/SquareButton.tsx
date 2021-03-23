e7175f5833d4578799f41d12e564f25444d11c62
Daniel Karlkvist <danielkarlkvist@gmail.com>
2021-03-23 19:18:08 +0100

    Added a square button css

--- client/src/components/SquareButton.tsx
+++ client/src/components/SquareButton.tsx
@@ -5,7 +5,7 @@ Authors: André, (Charlie and Hanna)
 
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../css/profile.css';
+import '../css/square-button.css';
 
 interface Props {
     label: string;
@@ -17,18 +17,18 @@ interface Props {
 
 export const SquareButton = (props: Props) => {
     const iconElem = props.icon === undefined ? <></> : <img src={props.icon} width="40%" height="40%" />;
-    const labelElem = props.label === undefined ? <></> : <h2 className="profile-button-names">{props.label}</h2>;
+    const labelElem = props.label === undefined ? <></> : <h2 className="square-button-text">{props.label}</h2>;
 
     return (
         <div>
             {props.linkTo === undefined ?
-                <button className={props.className} onClick={() => props.onClick()}>
+                <button className={`${props.className} ${"square-button"}`} onClick={() => props.onClick()}>
                     {iconElem}
                     {labelElem}
                 </button>
                 :
                 <Link to={props.linkTo}>
-                    <button className={props.className} onClick={() => props.onClick()}>
+                    <button className={`${props.className} ${"square-button"}`} onClick={() => props.onClick()}>
                         {iconElem}
                         {labelElem}
                     </button>
