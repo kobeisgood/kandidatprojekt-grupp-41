31581ebc9b0e58c9109a209455f437759e736682
Hanna Kalldal <Hanna@kalldal.se>
2021-03-25 14:03:09 +0100

    login klart

--- client/src/pages/LoginView.tsx
+++ client/src/pages/LoginView.tsx
@@ -2,8 +2,10 @@ import { useState } from 'react';
 import { User } from '../Types';
 import { Login } from '../Connection';
 import { useHistory } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import { SaveButton } from '../components/SaveButton';
 import { TextInput } from '../components/TextInput';
+import { BackButton } from '../components/BackButton';
 import '../css/login-view.css';
 
 
@@ -15,6 +17,9 @@ interface Props {
 }
 
 export const LoginView = (props: Props) => {
+    const ButtonNameClicked = () => {
+
+    }
     const
         [phoneInp, setPhoneInp] = useState(""),
         [passwordInp, setPasswordInp] = useState(""),
@@ -33,23 +38,39 @@ export const LoginView = (props: Props) => {
         props.setSocket(
             Login(phoneInp, passwordInp, props.setMe, redir)
         );
+
     };
 
     return (
-        <form onSubmit={(event) => event.preventDefault()}>
-            <div className="login-container">
-                <TextInput className="text-input-password" type="tel" label="Ditt mobilnummer: " 
-                placeholder="Skriv ditt mobilnummer här..."></TextInput>
-                <TextInput className="text-input-password" type="password" label="Ditt lösenord: " 
-                placeholder="Skriv ditt lösenord här..."></TextInput>
-            
-                <label className="container">Håll mig inloggad
+        <div>
+            <header className="login-header-container">
+                <div className="back-button-container">
+                    <Link to="/">
+                        <BackButton buttonFunction={ButtonNameClicked} />
+                    </Link>
+                </div>
+                <h1 className="login-header">Logga in</h1>
+            </header>
+            <div className= "description-text-container">
+                <h2 className="description-text">
+                    Fyll i fälten nedan och tryck sedan på “Logga in” för att gå vidare.
+            </h2>
+            </div>
+            <form onSubmit={(event) => event.preventDefault()}>
+                <div className="login-container">
+                    <TextInput className="text-input-password" type="tel" label="Ditt mobilnummer: "
+                        placeholder="Skriv ditt mobilnummer här..."></TextInput>
+                    <TextInput className="text-input-password" type="password" label="Ditt lösenord: "
+                        placeholder="Skriv ditt lösenord här..."></TextInput>
+
+                    <label className="container">Håll mig inloggad
                     <input type="checkbox"></input>
                         <span className="checkmark"></span>
-                </label>
-            </div>
-            <SaveButton label="Logga in" buttonFunction={attemptLogin} linkTo="/startView" />
-            
-        </form>
+                    </label>
+                </div>
+                <SaveButton label="Logga in" buttonFunction={attemptLogin} linkTo="/startView" />
+
+            </form>
+        </div>
     );
 };
--- client/src/pages/Start.tsx
+++ client/src/pages/Start.tsx
@@ -5,6 +5,8 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../css/start.css';
 import { SquareButton } from '../components/SquareButton';
+import cam_girl from "../images/cam_girl.png"
+
 
 
 
@@ -20,9 +22,10 @@ export const Start = () => {
                 Välkommen till Boom
             </h1>
             <div className="buttons-container">
-                <SquareButton label="Skapa konto" onClick={() => void 0} linkTo="/startView/" className="create-acount-button" />
                 <SquareButton label="Logga in" onClick={() => void 0} linkTo="/loginView/" className="login-button" />
+                <SquareButton label="Skapa konto" onClick={() => void 0} linkTo="/startView/" className="create-acount-button" />
             </div>
+            <img className="" src={cam_girl} alt="profilbild" />
         </div>
 
 
