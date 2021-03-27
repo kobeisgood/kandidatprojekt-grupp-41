e59a9eabb60ee8785a8fe168ecf2dfeaec1df9dc
Daniel Karlkvist <danielkarlkvist@gmail.com>
2021-03-27 19:05:46 +0100

    Added a ContactCardBig component

---
+++ client/src/components/ContactCardBig.tsx
@@ -0,0 +1,28 @@
+/* 
+    Component for a big contact card
+    Authors: Daniel and Robin
+ */
+
+import React from 'react';
+import '../css/contact-card.css';
+import callIcon from '../icons/call-icon.svg';
+import { SquareButton } from './SquareButton';
+import hjordis from '../images/hjordis.jpg';
+
+export const ContactCardBig = () => {
+    return (
+        <div className="contact-card-container-big">
+            <div className="contact-card-flexbox">
+                <img className="contact-card-profile-picture-big" src={hjordis} alt="Profilbild" />
+                <p className="contact-name-big">Repo <span>Laufsson</span></p>
+                {/* TODO
+                    - Add call function on click
+                    */}
+                <SquareButton label="Ring" onClick={() => void 0} icon={callIcon} className="call-button-big"/>
+            </div>
+        </div>
+    );
+}
+    
+    
+    
