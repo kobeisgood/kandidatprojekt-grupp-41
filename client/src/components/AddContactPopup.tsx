ec8d03a2b0b7ba5fa8ca03ab0cfc11d661b2563f
Theodor Khademi <theo.khademi@gmail.com>
2021-04-06 15:35:24 +0200

    Validation logic and gui fixed when searching contact

--- client/src/components/AddContactPopup.tsx
+++ client/src/components/AddContactPopup.tsx
@@ -26,17 +26,25 @@ export const AddContactPopup = (props: Props) => {
         [neutralPageState, setNeutralPageState] = useState(true),
         [phoneNumberInput, setPhoneNumberInput] = useState(""),
         [faultyNumberDisplayed, setFaultyNumberDisplayed] = useState(""),
-        [contactAddedState, setContactAddedState] = useState(false)
+        [contactAddedState, setContactAddedState] = useState(false),
+        [incorrectNumberState, setIncorrectNumberState] = useState(false),
+        [ownNumber, setOwnNumber] = useState(false)
 
     const handlePhoneNumberInput = (event: any) => {
         setPhoneNumberInput(event.target.value);
     }
 
+    const resetStates = () => {
+        setNeutralPageState(true)
+        setContactAddedState(false)
+        setIncorrectNumberState(false)
+        setOwnNumber(false)
+    }
+
     // Closes the add contact popup
     const closeAddContactPopup = () => {
         props.visibilityHandler();
-        setNeutralPageState(true)
-        setContactAddedState(false)
+        resetStates()
     };
 
     const [foundContact, setFoundContact] = useState({
@@ -51,6 +59,9 @@ export const AddContactPopup = (props: Props) => {
     // Searches for contact in db, renders correct content in popup
     const searchContact = () => {
 
+        setIncorrectNumberState(false)
+        setOwnNumber(false)
+
         setFaultyNumberDisplayed(phoneNumberInput)
 
         let contactNumber: string = phoneNumberInput
@@ -58,24 +69,29 @@ export const AddContactPopup = (props: Props) => {
 
         // When you try to add yourself
         if (contactNumber == props.phoneNumber) {
-            alert("Du försöker lägga till dig själv dumhuve, försök med ett annat nummer")
-            return
+            setNeutralPageState(false)
+            setIncorrectNumberState(true)
+            setOwnNumber(true)
+           // alert("Du försöker lägga till dig själv dumhuve, försök med ett annat nummer")
+           // return
         }
 
         // When you try to add someone you already have in your contacts
-        let foundBadNumber: boolean = false;
+        //let foundBadNumber: boolean = false;
         let i;
         for (i = 0; i < props.contactList.length; i++) {
             var contact = props.contactList[i]
             if (contact.phoneNbr == contactNumber) {
-                foundBadNumber = true;
-                alert("Den här kontakten finns redan i din kontaktlista.... herrejevlar kmr du int håg nåting?")
-                break;
+                setNeutralPageState(false)
+                setIncorrectNumberState(true)
+               // foundBadNumber = true;
+               // alert("Den här kontakten finns redan i din kontaktlista.... herrejevlar kmr du int håg nåting?")
+               // break;
             }
         }
-        if (foundBadNumber) {
+       /* if (foundBadNumber) {
             return
-        }
+        }*/
 
         if (props.socket != null) {
             GetSearchedContact(props.socket, contactNumber, setFoundContact)
@@ -132,8 +148,24 @@ export const AddContactPopup = (props: Props) => {
                     </>
                 }
 
+                {/* Faulty Contact found(own number or someone already in list)*/}
+                {foundContact != null && !neutralPageState && !contactAddedState && incorrectNumberState &&
+                    <>
+                        <p className="popup-error-message">Fel Nummer! </p>
+
+                        {ownNumber ? <p className="popup-middle-sized-text">Nummer {faultyNumberDisplayed} är ditt egna nummer</p> : 
+                            <p className="popup-middle-sized-text">Nummer {faultyNumberDisplayed} finns redan i din kontaktlista </p>}
+
+                        <p className="popup-middle-sized-text bottom-buffer">Kontrollera att du har skrivit rätt </p>
+                        <div className="number-input-row">
+                            <TextInput className="text-input w-400" label="Mobilnummer:" type="text" placeholder="Skriv mobilnummer här..." onChange={handlePhoneNumberInput} maxLength={10} /> 
+                        </div>
+                        <SquareButton label="Sök efter Boom kontakt" onClick={searchContact} className="save-button handle-contact-button button" />
+                    </>
+                }
+
                 {/* Contact found */}
-                {foundContact != null && !neutralPageState && !contactAddedState &&
+                {foundContact != null && !neutralPageState && !contactAddedState && !incorrectNumberState && 
                     <>
                         <div className="contact-found-row">
                             <img className="contact-card-profile-picture" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Edward_blom.melodifestivalen2018.18d873.1460307.jpg/1200px-Edward_blom.melodifestivalen2018.18d873.1460307.jpg" alt="KontaktBild" />
@@ -147,7 +179,7 @@ export const AddContactPopup = (props: Props) => {
                 }
 
                 {/* Contact added feedback */}
-                {foundContact != null && !neutralPageState && contactAddedState &&
+                {foundContact != null && !neutralPageState && contactAddedState && !incorrectNumberState &&
                     <>
                         <h4 className="popup-middle-sized-text bottom-buffer"> {foundContact.firstName} {foundContact.lastName} är nu tillagd i din telefonbok </h4>
                         <SquareButton label="Tillbaka till telefonboken" onClick={addContactFrontend} className="save-button handle-contact-button button" />
