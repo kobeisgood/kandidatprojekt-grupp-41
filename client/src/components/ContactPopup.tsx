bce5abeb281bdc8fc29a4d5b843680f85d7b5878
Theodor Khademi <theo.khademi@gmail.com>
2021-04-12 11:46:02 +0200

    Created contact popup component

---
+++ client/src/components/ContactPopup.tsx
@@ -0,0 +1,18 @@
+import { Contact } from "../Types";
+
+import '../css/popups.css';
+import '../css/colors.css';
+import '../css/contact-card.css';
+import '../css/carousel.css';
+
+import darkCrossIcon from '../icons/dark-cross-icon.svg';
+
+interface Props {
+    contact: Contact | undefined, 
+    visibilityHandler: Function
+}
+
+
+export const ContactPopup = (props:Props) => {
+
+}
